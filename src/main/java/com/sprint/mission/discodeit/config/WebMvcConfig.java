84031a167a2c015f513b066d4b174cb3382dbd39
NaNHangBok <kms_1015@naver.com>
2025-08-14 18:10:34 +0900

    sprint 7 과제 제출

---
+++ src/main/java/com/sprint/mission/discodeit/config/WebMvcConfig.java
@@ -0,0 +1,13 @@
+package com.sprint.mission.discodeit.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebMvcConfig {
+
+    @Bean
+    public MDCLoggingInterceptor mdcLoggingInterceptor() {
+        return new MDCLoggingInterceptor();
+    }
+}
