6a4487d54ed4550be1ea6f37db242631696060f3
NaNHangBok <kms_1015@naver.com>
2025-11-06 09:14:44 +0900

    중복 파일 삭제

--- src/main/java/com/sprint/mission/discodeit/dto/JwtDto.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.discodeit.dto;
-
-public record JwtDto(
-        UserDto userDto,
-        String accessToken
-) {
-}
--- src/main/java/com/sprint/mission/discodeit/dto/response/TokenResponse.java
+++
@@ -1,7 +0,0 @@
-package com.sprint.mission.discodeit.dto.response;
-
-public record TokenResponse(
-        String accessToken,
-        String refreshToken
-) {
-}
